2df6c898983c4f60ccc0c42e6c8e4345ab5932d4
Ajitama <fardan.nozami@gmail.com>
2025-03-28 03:09:28 +0700

    feat: basic type data

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+	var firstName string = "Fardan "
+	var lastName string = "Nozami"
+	var name = firstName + lastName
+	fmt.Println(len(name))
+	var isBoolean bool = false
+	fmt.Println(isBoolean)
+	fmt.Println(!false)
+	var int8 int = 32900
+	fmt.Println(int8)
+	var a = 4
+	b := 5
+	fmt.Println(a + b)
+	fmt.Println(a * b)
+}
