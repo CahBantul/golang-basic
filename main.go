c3374a524f91acaeb80f7facf79dcbb0c3b6b47a
Ajitama <fardan.nozami@gmail.com>
2025-04-09 16:16:30 +0700

    feat: if else dan switch

--- main.go
+++ main.go
@@ -3,6 +3,40 @@ package main
 import "fmt"
 
 func main() {
+	// if else
+	// angka := 9
+
+	// if angka%2 == 0 {
+	// 	fmt.Println("Genap")
+	// } else {
+	// 	fmt.Println("Ganjil")
+	// }
+
+	// multiple if
+	nilai := 90
+
+	if nilai >= 90 {
+		fmt.Println("A")
+	} else if nilai >= 75 {
+		fmt.Println("B")
+	} else if nilai >= 60 {
+		fmt.Println("C")
+	} else {
+		fmt.Println("D")
+	}
+
+	// switch
+	switch {
+	case nilai >= 90:
+		fmt.Println("A")
+	case nilai >= 75:
+		fmt.Println("B")
+	case nilai >= 60:
+		fmt.Println("C")
+	default:
+		fmt.Println("D")
+	}
+
 	// fmt.Println("Hello World")
 	// var firstName string = "Fardan "
 	// var lastName string = "Nozami"
@@ -17,19 +51,19 @@ func main() {
 	// b := 5
 	// fmt.Println(a + b)
 	// fmt.Println(a * b)
-	var numbers []string
-	numbers = append(numbers, "satu", "dua", "tiga")
-	fmt.Println(numbers[0:2])
+	// var numbers []string
+	// numbers = append(numbers, "satu", "dua", "tiga")
+	// fmt.Println(numbers[0:2])
 
-	person := make(map[string]string)
-	person["name"] = "ajitama"
-	person["job"] = "developer"
-	fmt.Println(person)
+	// person := make(map[string]string)
+	// person["name"] = "ajitama"
+	// person["job"] = "developer"
+	// fmt.Println(person)
 
-	students := map[string]int{
-		"Nozami": 90,
-		"Fardan": 70,
-	}
+	// students := map[string]int{
+	// 	"Nozami": 90,
+	// 	"Fardan": 70,
+	// }
 
-	fmt.Println(students)
+	// fmt.Println(students)
 }
